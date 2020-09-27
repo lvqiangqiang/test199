4801664e26ac45b8544ab640d3fa5a9bc7433216
lvqiang <1451022590@qq.com>
2020-09-27 16:32:45 +0800

    test1

--- src/main/java/com/kkgs/test/annotation/Test.java
+++ src/main/java/com/kkgs/test/annotation/Test.java
@@ -8,6 +8,15 @@ package com.kkgs.test.annotation;
 public class Test {
 
     public static void main(String[] args) {
-        FruitUtill.getFruitInfo(Apple.class);
+//        FruitUtill.getFruitInfo(Apple.class);
+
+    if ("个体工商户-时尚-礼品/鲜花/纪念品".contains("个体工商户")){
+        System.out.println("11111111111");
+    }else{
+        System.out.println("2222222222222222");
     }
+
+    }
+
+
 }
