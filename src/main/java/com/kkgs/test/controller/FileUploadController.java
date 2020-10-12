74980323ba26b91c0666a7aaf96d4bc555f9d29a
lvqiang <1451022590@qq.com>
2020-10-12 10:02:43 +0800

    test

---
+++ src/main/java/com/kkgs/test/controller/FileUploadController.java
@@ -0,0 +1,17 @@
+package com.kkgs.test.controller;
+
+import com.kkgs.test.po.Result;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+
+/**
+ * @Auther: lvqiang
+ * @Date: 2020/06/09/18:46
+ * @Description:
+ */
+@Controller
+public class FileUploadController {
+
+
+}
